d3925543779f3d0e015d03b407606a4a70f85b21
Suresh-48 <83566549+Suresh-48@users.noreply.github.com>
2022-01-08 01:33:02 +0530

    test

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# kharphi-frontend
--- src/constants/roles.js
+++
@@ -1,7 +0,0 @@
-export const ROLES_PARENT = "parent";
-
-export const ROLES_STUDENT = "student";
-
-export const ROLES_ADMIN = "admin";
-
-export const ROLES_TEACHER= "teacher";
